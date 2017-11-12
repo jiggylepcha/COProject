a920691a1e282db0bd21fb5abe91dbc73d9af10e
Suryatej Reddy <suryatej16102@iiitd.ac.in>
2017-11-12 21:34:43 +0400

    added basic class

--- COProject.py
+++ COProject.py
@@ -1,19 +1,68 @@
 #CO Project
 #Arm Simulator
 
+
+class Instruction:
+	all_instructions = list()
+	def __init__(self,addr,inst):
+		self.addressInHex = addr
+		self.addressInInt = getIntFromHex(addr)
+		self.instruction = inst
+		Instruction.all_instructions.append(self)
+
+	@staticmethod
+	def getInstruction(address):
+		return next((instruct for instruct in Instruction.all_instructions if instruct.addressInInt == address), None)
+
+	def printFetchStatement(self):
+		print "Fetch instruction " + self.instruction + " from address " + self.addressInHex
+
+	def decodeInstruction(self):
+		pass
+
+
 def getIntFromHex(hexValue):
 	return int(hexValue,16)
 
+
+#returns dictionary
+def initRegisters(numberOfRegisters = 32):
+	registers = dict()
+	for reigsterId in range(1,33):
+		registers[reigsterId] = 0
+	return registers
+
+#returns List
+def initMainMemory():
+	memory = list()
+	return memory
+
+
+#the dictionary contains {0:('0x0xE3A0200A',0x0),4:('0x0xE3A0200A',0x4)}
 def loadFromFile(fileName):
-	instructions = dict()
 	file = open(fileName,'r')
 	allInstructions = file.readlines()
 	for data in allInstructions:
 		instruct = data.split()
 		addressInHex = instruct[0].strip()
-		addressInInt = getIntFromHex(addressInHex)
 		instruction = instruct[1].strip()
-		instructions[addressInInt] = instruction
-	print instructions
+		tempInstruction = Instruction(addressInHex,instruction)
+
+#just prints instruction
+def fetchInstruction(instLocation):
+	curInstruction = Instruction.getInstruction(instLocation)
+	curInstruction.printFetchStatement()
 
 
+#takes the instruction object as a parameter
+def decodeInstruction(instruction):
+	pass
+
+
+def main():
+	loadFromFile("input.mem")
+	for i in range(0,13,4):
+		fetchInstruction(i)
+
+if __name__=='__main__':
+	main()
